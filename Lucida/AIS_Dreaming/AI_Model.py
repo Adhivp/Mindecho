3fbec69e0bf5bce95f8f0be31fb1218fed1ead3e
Adhivp <adhivp910@gmail.com>
2023-11-25 02:12:39 +0530

    Frontend of Dreamvista_journal

--- Lucida/AIS_Dreaming/AI_Model.py
+++ Lucida/AIS_Dreaming/AI_Model.py
@@ -1,136 +1,89 @@
 import numpy as np
 import PIL.Image
 import tensorflow as tf
+import matplotlib.pyplot as plt
 import urllib.request
 import os
 import zipfile
-import io
 
-def deep_dream_from_url(image_url):
-    # Helper functions for TF Graph visualization
-    def strip_consts(graph_def, max_const_size=32):
-        strip_def = tf.GraphDef()
-        for n0 in graph_def.node:
-            n = strip_def.node.add()
-            n.MergeFrom(n0)
-            if n.op == 'Const':
-                tensor = n.attr['value'].tensor
-                size = len(tensor.tensor_content)
-                if size > max_const_size:
-                    tensor.tensor_content = "<stripped %d bytes>" % size
-        return strip_def
-
-    def rename_nodes(graph_def, rename_func):
-        res_def = tf.GraphDef()
-        for n0 in graph_def.node:
-            n = res_def.node.add()
-            n.MergeFrom(n0)
-            n.name = rename_func(n.name)
-            for i, s in enumerate(n.input):
-                n.input[i] = rename_func(s) if s[0] != '^' else '^' + rename_func(s[1:])
-        return res_def
-
-    def showarray(a):
-        a = np.uint8(np.clip(a, 0, 1) * 255)
-        PIL.Image.fromarray(a).show()
-
-    def visstd(a, s=0.1):
-        return (a - a.mean()) / max(a.std(), 1e-4) * s + 0.5
-
-    def T(layer):
-        return graph.get_tensor_by_name("import/%s:0" % layer)
-
-    def render_naive(t_obj, img0=img_noise, iter_n=20, step=1.0):
-        t_score = tf.reduce_mean(t_obj)
-        t_grad = tf.gradients(t_score, t_input)[0]
-
-        img = img0.copy()
-        for _ in range(iter_n):
-            g, _ = sess.run([t_grad, t_score], {t_input: img})
-            g /= g.std() + 1e-8
-            img += g * step
-        showarray(visstd(img))
-
-    def tffunc(*argtypes):
-        placeholders = list(map(tf.placeholder, argtypes))
-        def wrap(f):
-            out = f(*placeholders)
-            def wrapper(*args, **kw):
-                return out.eval(dict(zip(placeholders, args)), session=kw.get('session'))
-            return wrapper
-        return wrap
-
-    def resize(img, size):
-        img = tf.expand_dims(img, 0)
-        return tf.image.resize_bilinear(img, size)[0, :, :, :]
-    resize = tffunc(np.float32, np.int32)(resize)
-
-    def calc_grad_tiled(img, t_grad, tile_size=512):
-        sz = tile_size
-        h, w = img.shape[:2]
-        sx, sy = np.random.randint(sz, size=2)
-        img_shift = np.roll(np.roll(img, sx, 1), sy, 0)
-        grad = np.zeros_like(img)
-        for y in range(0, max(h - sz // 2, sz), sz):
-            for x in range(0, max(w - sz // 2, sz), sz):
-                sub = img_shift[y:y + sz, x:x + sz]
-                g = sess.run(t_grad, {t_input: sub})
-                grad[y:y + sz, x:x + sz] = g
-        return np.roll(np.roll(grad, -sx, 1), -sy, 0)
-
-    def render_deepdream(t_obj, img0=img_noise, iter_n=10, step=1.5, octave_n=4, octave_scale=1.4):
-        t_score = tf.reduce_mean(t_obj)
-        t_grad = tf.gradients(t_score, t_input)[0]
-
-        img = img0
-        octaves = []
-        for _ in range(octave_n - 1):
-            hw = img.shape[:2]
-            lo = resize(img, np.int32(np.float32(hw) / octave_scale))
-            hi = img - resize(lo, hw)
-            img = lo
-            octaves.append(hi)
-
-        for octave in range(octave_n):
-            if octave > 0:
-                hi = octaves[-octave]
-                img = resize(img, hi.shape[:2]) + hi
-            for _ in range(iter_n):
-                g = calc_grad_tiled(img, t_grad)
-                img += g * (step / (np.abs(g).mean() + 1e-7))
-            showarray(img / 255.0)
-
-    # Step 1 - Download image from the given URL
-    img_content = urllib.request.urlopen(image_url).read()
-    img0 = PIL.Image.open(io.BytesIO(img_content))
-    img0 = np.float32(img0)
-
-    img_noise = np.random.uniform(size=img0.shape) + 100.0
-    # Step 2 - Download and set up the Inception model
+def main():
+    # Step 1 - download google's pre-trained neural network
     url = 'https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip'
     data_dir = '../data/'
     model_name = os.path.split(url)[-1]
     local_zip_file = os.path.join(data_dir, model_name)
     if not os.path.exists(local_zip_file):
+        # Download
         model_url = urllib.request.urlopen(url)
         with open(local_zip_file, 'wb') as output:
             output.write(model_url.read())
+        # Extract
         with zipfile.ZipFile(local_zip_file, 'r') as zip_ref:
             zip_ref.extractall(data_dir)
 
-    # Step 3 - Creating Tensorflow session and loading the model
+    # start with a gray image with a little noise
+    img_noise = np.random.uniform(size=(224, 224, 3)) + 100.0
+
+    model_fn = 'tensorflow_inception_graph.pb'
+
+    # Step 2 - Creating Tensorflow session and loading the model
     graph = tf.Graph()
     sess = tf.InteractiveSession(graph=graph)
-    with tf.gfile.FastGFile(os.path.join(data_dir, 'tensorflow_inception_graph.pb'), 'rb') as f:
+    with tf.gfile.FastGFile(os.path.join(data_dir, model_fn), 'rb') as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
-    t_input = tf.placeholder(np.float32, name='input')
+    t_input = tf.placeholder(np.float32, name='input')  # define the input tensor
     imagenet_mean = 117.0
     t_preprocessed = tf.expand_dims(t_input - imagenet_mean, 0)
     tf.import_graph_def(graph_def, {'input': t_preprocessed})
 
-    # Step 4 - Apply gradient ascent to that layer
-    render_deepdream(tf.square(T('mixed4c')), img0)
+    layers = [op.name for op in graph.get_operations() if op.type == 'Conv2D' and 'import/' in op.name]
+    feature_nums = [int(graph.get_tensor_by_name(name + ':0').get_shape()[-1]) for name in layers]
 
-# Example usage:
-deep_dream_from_url('https://example.com/path/to/your/image.jpg')
+    print('Number of layers', len(layers))
+    print('Total number of feature channels:', sum(feature_nums))
+
+    # Step 3 - Pick a layer to enhance our image
+    layer = 'mixed4d_3x3_bottleneck_pre_relu'
+    channel = 139  # picking some feature channel to visualize
+
+    # open image
+    img0 = PIL.Image.open('pilatus800.jpg')
+    img0 = np.float32(img0)
+
+    # Step 4 - Apply gradient ascent to that layer
+    t_score = tf.reduce_mean(tf.square(graph.get_tensor_by_name("import/%s:0" % layer)))
+    t_grad = tf.gradients(t_score, t_input)[0]
+
+    # Split the image into a number of octaves
+    img = img0
+    octaves = []
+    octave_n = 4
+    octave_scale = 1.4
+    for _ in range(octave_n - 1):
+        hw = img.shape[:2]
+        lo = resize(img, np.int32(np.float32(hw) / octave_scale))
+        hi = img - resize(lo, hw)
+        img = lo
+        octaves.append(hi)
+
+    # Generate details octave by octave
+    for octave in range(octave_n):
+        if octave > 0:
+            hi = octaves[-octave]
+            img = resize(img, hi.shape[:2]) + hi
+        iter_n = 10
+        step = 1.5
+        for _ in range(iter_n):
+            g = calc_grad_tiled(img, t_grad)
+            img += g * (step / (np.abs(g).mean() + 1e-7))
+
+        # This will usually be like 3 or 4 octaves
+        # Step 5 output deep dream image via matplotlib
+        showarray(img / 255.0)
+
+
+# Helper functions and other code removed for brevity
+
+if __name__ == '__main__':
+    main()
